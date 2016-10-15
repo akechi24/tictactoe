624bde85fbdc62fb3812c9f418b7f89ae0df4229
akechi24 <tomasz.zamojc@gmail.com>
2016-10-15 16:48:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+﻿var Game = require('./modules/Game');
+
+window.initGame = function(){
+	if(typeof this.gameLoop !== 'undefined') this.cancelAnimationFrame(this.gameLoop);
+	var game = new Game();
+	game.init();
+}
+
+initGame();
